fecbab1069fb39651ef7cb89a8183d3ad1f45b71
XiaoxiaNSCC <w0471965@nscc.ca>
2024-02-23 21:46:27 -0400

    ready for marking

--- main.js
+++ main.js
@@ -20,13 +20,13 @@
               if (data.hasOwnProperty(key)) {
                 if(key==="common"){
                   data[key].forEach((item, index) => {
-                    var name_p = document.createElement('p');
-                    name_p.innerHTML =key + index +' '+ item.food_name
-                    document.getElementById('body').appendChild(name_p); 
+                    var name_p = document.createElement('span');
+                    name_p.innerHTML =key+" "+ (index+1) +' '+ item.food_name
+                    document.getElementById('foodInfo').appendChild(name_p); 
 
                     var img = document.createElement('img');
                     img.src = item.photo.thumb;
-                    document.getElementById('body').appendChild(img);        
+                    document.getElementById('foodInfo').appendChild(img);        
                     console.log(item.photo.thumb)        
                   });
                 }             
@@ -39,16 +39,13 @@
     
     window.btnLoad = btnLoad;
 
+    //Park C Poker Hands
     var poker_url = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
     fetch(poker_url)
         .then (function(response){
             return response.json();
         })
         .then (function (data){
-            if(data.success ===true){
-                console.log("api fetch ok");
-                console.log(data.deck_id)
-            }
             var deck_id = data.deck_id;
             var card_count = 5;
             var poker_draw_url = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${card_count}`
@@ -57,20 +54,19 @@
                 return response.json();
             })
             .then(function(data){
-                console.log(data.cards);
                 data.cards.forEach((card)=>{
                     var img = document.createElement('img');
                     img.src = card.image;
-                    document.getElementById('body').appendChild(img); 
+                    document.getElementById('card').appendChild(img); 
                 })
                 var high_card = determineHighestHand(data.cards);
                 var high_card_display = document.createElement('h4');
-                high_card_display.innerHTML = high_card;
-                document.getElementById('body').appendChild(high_card_display); 
+                high_card_display.innerHTML = "Initial Poken Hands is " + high_card;
+                document.getElementById('card').appendChild(high_card_display); 
             });
         })
 
-    //Async fetch function
+    //Test function
 
     const royal_flush = 'https://prog2700.onrender.com/pokerhandtest/royalflush';
     const straight_flush = 'https://prog2700.onrender.com/pokerhandtest/straightflush';
@@ -87,11 +83,8 @@
     async function poker_hands_call(url){
         const response = await fetch(url);
         const re_cards = await response.json();
-        console.log(re_cards);
-        console.log(re_cards.cards);
         return re_cards.cards;
     }
-
 
     function display(cards){
         cards.forEach((card)=>{
@@ -106,13 +99,118 @@
                 document.getElementById('display').appendChild(high_card_display); 
 
     }
+
+    //Royal Flush
+    function btnRF(){
+        poker_hands_call(royal_flush)
+        .then(cards=>{
+          display(cards);
+        })
+    }
+    window.btnRF = btnRF;
+
+    //Straight Flush
+    function btnSF(){
+        poker_hands_call(straight_flush)
+        .then(cards=>{
+        display(cards);
+        })
+    }
     
-  poker_hands_call(full_house)
-  .then(cards=>{
-    display(cards);
-  })
+    window.btnSF = btnSF;
 
+    //four_of_a_kind
+    function btnFK(){
+        poker_hands_call(four_of_a_kind)
+        .then(cards=>{
+        display(cards);
+        })
+    }
+    
+    window.btnFK = btnFK;
 
+    //Full House
+    function btnFH(){
+        poker_hands_call(full_house)
+        .then(cards=>{
+        display(cards);
+        })
+    }
+
+    window.btnFH = btnFH;
+
+    //Flush
+    function btnFlush(){
+        poker_hands_call(flush)
+        .then(cards=>{
+        display(cards);
+        })
+    }
+    
+    window.btnFlush = btnFlush;
+
+    //straight
+    function btnStr(){
+        poker_hands_call(straight)
+        .then(cards=>{
+        display(cards);
+        })
+    }
+    
+    window.btnStr = btnStr;
+
+    //three_of_a_kind
+    function btnTK(){
+        poker_hands_call(three_of_a_kind)
+        .then(cards=>{
+        display(cards);
+        })
+    }
+    
+    window.btnTK = btnTK;
+
+    //two_pair
+    function btnTP(){
+        poker_hands_call(two_pair)
+        .then(cards=>{
+        display(cards);
+        })
+    }
+    
+    window.btnTP = btnTP;
+
+    //one_pair
+    function btnOP(){
+        poker_hands_call(one_pair)
+        .then(cards=>{
+        display(cards);
+        })
+    }
+    
+    window.btnOP = btnOP;
+
+    //high_card
+    function btnHC(){
+        poker_hands_call(high_card)
+        .then(cards=>{
+        display(cards);
+        })
+    }
+    
+    window.btnHC = btnHC;
+
+    //random_hand
+    function btnRH(){
+        poker_hands_call(random)
+        .then(cards=>{
+        display(cards);
+        })
+    }
+    
+    window.btnRH = btnRH;
+    
+
+    //Question 4	WRITE A FUNCTION THAT WILL DETERMINE THE HIGHEST POKER HAND 
     function determinRank(value){
         const faceCards = { JACK: 11, QUEEN: 12, KING: 13, ACE: 14 };
         return faceCards[value] || parseInt(value);
@@ -122,8 +220,15 @@
         // Sort cards by rank
         cards.sort((a, b) => determinRank(a.value) -  determinRank(b.value));
     
+
         // Check for specific hand combinations
+        if (isRoyalFlush(cards)) {
+            console.log(cards)
+            return "Royal Flush";
+        }
+
         if (isStraightFlush(cards)) {
+            console.log(cards)
             return "Straight Flush";
         }
         if (isFourOfAKind(cards)) {
@@ -171,9 +276,13 @@
         return cards.every(card => card.suit === cards[0].suit);
     }
     
+    function isRoyalFlush(cards) {
+        return cards.every(card =>determinRank(card.value)>=10) && cards.every(card => card.suit === cards[0].suit);
+    }
+
     function isStraight(cards) {
-        for (let i = 1; i < cards.length; i++) {
-            if (determinRank(cards[i].value) !== determinRank(cards[i - 1].value + 1)) {
+        for (let i = 1; i < cards.length; i++) {           
+            if (determinRank(cards[i].value) !== determinRank(cards[i - 1].value)+1) {
                 return false;
             }
         }
